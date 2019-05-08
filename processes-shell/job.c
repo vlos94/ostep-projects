87fd0e565588d1dad0b0e6a904b1912eefbb8ad8
Vicente Lossada <38084081+vlos94@users.noreply.github.com>
2019-05-08 10:19:09 -0700

    Update job.c

--- processes-shell/job.c
+++ processes-shell/job.c
@@ -4,6 +4,8 @@
 #include "cmd.h"
 #include "path.h"
 #include "errors.h"
+
+
 /*Run jobs, ie a line entered to the shell
   A job is a line of cmds separated by &'s
   job ---> cmd_1 & ... & cmd_n 
@@ -11,7 +13,6 @@
   A return value of 0 will mean to
   keep looping for input
   A return value of 1 will mean exit*/
-
 
 int run_job (char* input_line, node** path) {
   char** job_tokens = parse_tokens (input_line, "&");
@@ -24,21 +25,26 @@ int run_job (char* input_line, node** path) {
       ++offset;
       continue;
     }
+    
     char* cmd = get_cmd (job_tokens[offset]);
     char* file_name = get_file_name (job_tokens[offset]);
     status = run_cmd (cmd, file_name, path);
+    
     if (status == 1) {
       free (cmd);
       free (file_name);
       free_tokens (job_tokens);
       return 1;
     }
+    
     if (status == -1)
       error_msg();
+    
     ++offset;
     free (cmd);
     free (file_name);
   }
+  
   free_tokens (job_tokens);
   return 0;
 }
